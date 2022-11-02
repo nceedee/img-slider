43794a7a3253de293936600c3cc59a8980bcf52a
Callmebobonwa <nnajidavid58@gmail.com>
2022-11-02 22:48:38 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function scrollL()
+{
+    let left = document.querySelector( ".scroll-images" );
+    left.scrollBy(350,0)
+}
+
+function scrollR() {
+  let left = document.querySelector(".scroll-images");
+  left.scrollBy(-350, 0);
+}
